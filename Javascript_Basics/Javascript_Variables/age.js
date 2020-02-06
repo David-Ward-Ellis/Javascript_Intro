c5d2792192e21066b1e88b7970b1288dccf32bbf
David Ellis <ellisdb@bc.edu>
2020-02-06 10:20:40 -0500

    Practice with using variable declaration and manipulation

---
+++ Javascript_Basics/Javascript_Variables/age.js
@@ -0,0 +1,14 @@
+let age = 20;
+
+// My initial solution
+// console.log("In 10 years, you will be" + " " + Number (age +  10) + " " + "years old");
+// console.log("In 20 years, you will be" + " " + Number (age +  20) + " " + "years old");
+// console.log("In 30 years, you will be" + " " + Number (age +  30) + " " + "years old");
+// console.log("In 40 years, you will be" + " " + Number (age +  40) + " " + "years old");    
+
+//The better solution
+console.log(`You are curently ${age} years old.`)
+console.log(`In 10 years, you will be ${age + 10} years old.`)
+console.log(`In 20 years, you will be ${age + 20} years old.`)
+console.log(`In 30 years, you will be ${age + 30} years old.`)
+console.log(`In 40 years, you will be ${age + 40} years old.`)
---
+++ Javascript_Basics/Javascript_Variables/greeter.js
@@ -0,0 +1,5 @@
+let greeted = "Victor";
+
+console.log(`Good morning, ${greeted}`);
+console.log(`Good afternoon, ${greeted}`);
+console.log(`Good night, ${greeted}`);
